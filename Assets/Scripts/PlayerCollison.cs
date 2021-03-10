f87005c2fa2d0d3cbd4fedae7af7a66d6babb607
Mustafa Battaloğlu <mmbattaloglu@gmail.com>
2021-03-10 19:18:16 +0300

    Cube Game

---
+++ Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit a Obstacle");
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+;
+        }
+    }
+}
